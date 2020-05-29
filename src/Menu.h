8d083061f6d369792825d24d8949a31163d18b2c
Mr-Fusion <joelturner128@gmail.com>
2020-05-29 11:10:20 -0700

    Development Rev 0.3.2
    - Implemented Settings sub menu
    TODO: Connect settings to gameplay

--- src/Menu.h
+++ src/Menu.h
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include "Button.h"
 
+#define LEFT_JUSTIFY    20
+#define RIGHT_JUSTIFY   SCREEN_WIDTH - 20
+
 enum {
     MENU_START_GAME = 0,
     MENU_SETTINGS = 1,
@@ -12,28 +15,53 @@ enum {
     MENU_SIZE = 3
 };
 
+enum {
+    SETTINGS_SOUND = 0,
+    SETTINGS_DIFFY = 1,
+    SETTINGS_MULTI = 2,
+    SETTINGS_BACK = 3,
+    SETTINGS_SIZE = 4
+};
+
+enum {
+    DIFFY_NORM = 0,
+    DIFFY_EASY = 1,
+    DIFFY_FREE = 2,
+    DIFFY_SIZE = 3
+};
+
 class Menu : public GameState
 {
     public:
 
-    bool retInput;
-    int curSelection;
+    bool retInput = false;
+    bool subMenu = false;
+    int curSelection = 0;
 
-    std::stringstream credit;
+    bool sfxEnable = true;
+    bool multiEnable = false;
+    int difficulty = DIFFY_NORM;
 
     LTexture titleText;
     LTexture startText;
     LTexture settingText;
     LTexture quitText;
+
+    LTexture soundText;
+    LTexture diffyText;
+    LTexture multiText;
+    LTexture backText;
+
+    LTexture soundTextValue;
+    LTexture diffyTextValue;
+    LTexture multiTextValue;
+
     LTexture creditText;
 
     SDL_Color textColor;
 
     ///Constructor Function
     Menu(){
-
-        retInput = false;
-        curSelection = 0;
 
         textColor = { 0xFF, 0xFF, 0xFF };
 
@@ -102,6 +130,7 @@ class Menu : public GameState
         {
             //Render text
             //Set text to be rendered
+            std::stringstream credit;
             credit.str( "" );
             credit << "By Joel Turner | Version: " << MAJOR_REV << "." << MINOR_REV << "." << INCREMENTAL_REV;
 
@@ -137,12 +166,57 @@ class Menu : public GameState
                 printf( "Failed to render quitText!\n" );
                 success = false;
             }
+
+            if( !soundText.loadFromRenderedText( "Sound Effects:", textColor ) )
+            {
+                printf( "Failed to render soundText!\n" );
+                success = false;
+            }
+            if( !diffyText.loadFromRenderedText( "Difficulty:", textColor ) )
+            {
+                printf( "Failed to render diffyText!\n" );
+                success = false;
+            }
+            if( !multiText.loadFromRenderedText( "All multiballs!", textColor ) )
+            {
+                printf( "Failed to render multiText!\n" );
+                success = false;
+            }
+            if( !backText.loadFromRenderedText( "Back", textColor ) )
+            {
+                printf( "Failed to render backText!\n" );
+                success = false;
+            }
+
+            if( !soundTextValue.loadFromRenderedText( "On", textColor ) )
+            {
+                printf( "Failed to render soundTextValue!\n" );
+                success = false;
+            }
+            if( !diffyTextValue.loadFromRenderedText( "Normal", textColor ) )
+            {
+                printf( "Failed to render diffyTextValue!\n" );
+                success = false;
+            }
+            if( !multiTextValue.loadFromRenderedText( "Off", textColor ) )
+            {
+                printf( "Failed to render multiTextValue!\n" );
+                success = false;
+            }
+
         }
         return success;
     }
     
     ///Handles mouse event
     void handleEvent( SDL_Event* e){
+
+        int size;
+
+        if (subMenu)
+            size = SETTINGS_SIZE;
+        else
+            size = MENU_SIZE;
 
         if (e->type == SDL_KEYDOWN) {
             switch (e->key.keysym.sym) {
@@ -152,19 +226,45 @@ class Menu : public GameState
                 case SDLK_w:
                     curSelection--;
                     if (curSelection < 0)
-                        curSelection = MENU_SIZE - 1;
+                        curSelection = size - 1;
                 break;
 
                 case SDLK_s:
                     curSelection++;
-                    if (curSelection >= MENU_SIZE )
-                        curSelection = MENU_START_GAME;
+                    if (curSelection >= size )
+                        curSelection = 0;
                 break;
             }
         }
     }
 
-    void logic(){
+    bool toggleText(LTexture *text, bool flag) {
+        if (flag) {
+            flag = false;
+            text->loadFromRenderedText( "Off", textColor );
+        }
+        else {
+            flag = true;
+            text->loadFromRenderedText( "On", textColor );
+        }
+        return flag;
+    }
+
+    void updateDiffy(int d){
+        switch (d) {
+            case DIFFY_NORM:
+                diffyTextValue.loadFromRenderedText( "Normal", textColor );
+            break;
+            case DIFFY_EASY:
+                diffyTextValue.loadFromRenderedText( "Easy", textColor );
+            break;
+            case DIFFY_FREE:
+                diffyTextValue.loadFromRenderedText( "Freeplay", textColor );
+            break;
+        }
+    }
+
+    void topMenu(){
 
         // Set all options to white text by default
         startText.setColor(0xFF, 0xFF, 0xFF);
@@ -190,7 +290,8 @@ class Menu : public GameState
                     set_next_state( STATE_GAME );
                 break;
                 case MENU_SETTINGS:
-                    //set_next_state( int newState );
+                    subMenu = true;
+                    curSelection = 0;
                 break;
                 case MENU_QUIT:
                     set_next_state( STATE_EXIT );
@@ -200,16 +301,92 @@ class Menu : public GameState
         }
     }
 
+    void settingsMenu(){
+
+        // Set all options to white text by default
+        soundText.setColor(0xFF, 0xFF, 0xFF);
+        diffyText.setColor(0xFF, 0xFF, 0xFF);
+        multiText.setColor(0xFF, 0xFF, 0xFF);
+        backText.setColor(0xFF, 0xFF, 0xFF);
+
+        soundTextValue.setColor(0xFF, 0xFF, 0xFF);
+        diffyTextValue.setColor(0xFF, 0xFF, 0xFF);
+        multiTextValue.setColor(0xFF, 0xFF, 0xFF);
+
+        if (retInput){
+            switch (curSelection) {
+                case SETTINGS_SOUND:
+                    sfxEnable = toggleText( &soundTextValue, sfxEnable);
+                break;
+                case SETTINGS_DIFFY:
+                    difficulty++;
+                    if (difficulty == DIFFY_SIZE)
+                        difficulty = DIFFY_NORM;
+                    updateDiffy( difficulty );
+                break;
+                case SETTINGS_MULTI:
+                    multiEnable = toggleText( &multiTextValue, multiEnable);
+                break;
+                case SETTINGS_BACK:
+                    subMenu = false;
+                    curSelection = 0;
+                break;
+            }
+            retInput = false;
+        }
+
+        // Highlight the appropriate option in red corresponding to the current cursor selection
+        switch (curSelection) {
+            case SETTINGS_SOUND:
+                soundText.setColor(0xFF, 0x00, 0x00);
+                soundTextValue.setColor(0xFF, 0x00, 0x00);
+            break;
+            case SETTINGS_DIFFY:
+                diffyText.setColor(0xFF, 0x00, 0x00);
+                diffyTextValue.setColor(0xFF, 0x00, 0x00);
+            break;
+            case SETTINGS_MULTI:
+                multiText.setColor(0xFF, 0x00, 0x00);
+                multiTextValue.setColor(0xFF, 0x00, 0x00);
+            break;
+            case SETTINGS_BACK:
+                backText.setColor(0xFF, 0x00, 0x00);
+            break;
+        }
+    }
+
+    void logic(){
+
+        if ( subMenu )
+            settingsMenu();
+        else
+            topMenu();
+
+    }
+
     void render(){
 
         SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-        // SDL_RenderFillRect(gRenderer, &paddle);
 
         titleText.render(SCREEN_WIDTH/2 - titleText.getWidth()/2, 30);
-        startText.render(SCREEN_WIDTH/2 - startText.getWidth()/2, SCREEN_HEIGHT/2 - startText.getHeight() );
-        settingText.render(SCREEN_WIDTH/2 - settingText.getWidth()/2, SCREEN_HEIGHT/2);
-        quitText.render(SCREEN_WIDTH/2 - quitText.getWidth()/2, SCREEN_HEIGHT/2 + quitText.getHeight());
-        creditText.render(SCREEN_WIDTH/2 - creditText.getWidth()/2, SCREEN_HEIGHT - 60);
+
+        if ( subMenu ) {
+            soundText.render( LEFT_JUSTIFY, SCREEN_HEIGHT/2 - soundText.getHeight() );
+            diffyText.render( LEFT_JUSTIFY, SCREEN_HEIGHT/2 );
+            multiText.render( LEFT_JUSTIFY, SCREEN_HEIGHT/2 + multiText.getHeight() );
+            backText.render( LEFT_JUSTIFY, SCREEN_HEIGHT/2 + backText.getHeight() * 2);
+
+            soundTextValue.render( RIGHT_JUSTIFY - soundTextValue.getWidth(), SCREEN_HEIGHT/2 - soundTextValue.getHeight() );
+            diffyTextValue.render( RIGHT_JUSTIFY - diffyTextValue.getWidth(), SCREEN_HEIGHT/2 );
+            multiTextValue.render( RIGHT_JUSTIFY - multiTextValue.getWidth(), SCREEN_HEIGHT/2 + multiTextValue.getHeight() );
+        }
+        else {
+            startText.render(SCREEN_WIDTH/2 - startText.getWidth()/2, SCREEN_HEIGHT/2 - startText.getHeight() );
+            settingText.render(SCREEN_WIDTH/2 - settingText.getWidth()/2, SCREEN_HEIGHT/2);
+            quitText.render(SCREEN_WIDTH/2 - quitText.getWidth()/2, SCREEN_HEIGHT/2 + quitText.getHeight());
+            creditText.render(SCREEN_WIDTH/2 - creditText.getWidth()/2, SCREEN_HEIGHT - 60);
+        }
+
     }
 
 };
